72048cb359b4564760075be5997299895d09f27a
Adam Foulke <adam.foulke@gmail.com>
2023-07-02 17:57:24 -0400

    Added some inital styling based on other website

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
-import logo from './logo.svg';
+import logo from './assets/rings-icon-removebg-preview.png';
+import us from './assets/US.jpg';
 import './App.css';
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to AJ and Cassie's Wedding Website
-        </p>
+        <div className='Title-Font'> Adam and Cassie </div>
       </header>
-      <div>
-        <div className="App-body">
-          <Carousel>
-                <div>
-                    <img src="assets/1.jpeg" />
-                    <p className="legend">Legend 1</p>
-                </div>
-                <div>
-                    <img src="assets/2.jpeg" />
-                    <p className="legend">Legend 2</p>
-                </div>
-                <div>
-                    <img src="assets/3.jpeg" />
-                    <p className="legend">Legend 3</p>
-                </div>
-            </Carousel>
+      <div className="App-body">
+        <div className="box m-5">
+        A Story about us. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras bibendum, tellus et venenatis porta, leo tortor semper libero, eget cursus sapien purus quis lorem. Maecenas tincidunt diam id nibh facilisis sodales. Fusce efficitur orci vitae pellentesque scelerisque. Phasellus aliquam pharetra volutpat. In hac habitasse platea dictumst. Cras ut turpis ex. Duis eleifend venenatis magna non vehicula. Etiam porttitor orci sed quam tempor, eget facilisis tortor mollis. Donec auctor, lorem in facilisis porta, mi felis tincidunt ipsum, a ornare purus turpis sit amet nisi. Ut accumsan tincidunt pulvinar. Sed quis dapibus quam, ullamcorper mattis nisi. Fusce sapien velit, mollis nec enim vitae, commodo dapibus nisl. Nunc vel arcu quis sapien dapibus bibendum. Aliquam sit amet cursus risus. Vivamus sit amet dictum nunc, a iaculis ligula. Etiam bibendum luctus turpis pulvinar mattis.      
+        </div>
+        <div className="box m-3">
+          <img src={us} className="Us-Photo" alt="logo" />
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import TestComp from './TestComp'
+import LadiesAndGentlemen from './LadiesAndGentlemen'
+import NavBar from './NavBar'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <LadiesAndGentlemen />
   </React.StrictMode>
 );
 
